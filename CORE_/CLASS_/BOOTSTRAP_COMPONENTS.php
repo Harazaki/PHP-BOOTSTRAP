b0d75e188e2a5cd39e4f582c3dad9ac9492602d5
Septyaman Vom <septyvombervom@gmail.com>
2014-12-26 08:57:30 +0700

    Update BOOTSTRAP_COMPONENTS.php
    Add another components

--- CORE_/CLASS_/BOOTSTRAP_COMPONENTS.php
+++ CORE_/CLASS_/BOOTSTRAP_COMPONENTS.php
@@ -1,19 +1,17 @@
 <?php if( !defined('HARAZAKIDA') || !defined('BOOTSTRAP_COMPONENT') ) exit;
 
-  /* 
-       OPEN SOURCE PROJECTS © HARAZAKI 2014 
-	     THE PHP BOOTSTRAP_COMPONENT CLASSES
-	    - Learn more about at http://getbootstrap.com/components/
-
+  /*  
+     OPEN SOURCE PROJECTS © HARAZAKI 2014
+	 [THE PHP BOOTSTRAP_COMPONENT CLASSES]()
+	  - Learn more about at http://getbootstrap.com/components/
+	  - Notes : It's still just a skeleton , let's continue the development of the future,
+	  - You can contribut at https://github.com/Harazaki/PHP-BOOTSTRAP
   */  
+  
   
   Class BOOTSTRAP_COMPONENT {
   
-      function __construct(){	    
-	  
-	  }
-	  
-	  
+
 	  /*  * [ DROPDOWN  MENU ] *
 	  
 	      - $data must be an array, 
@@ -24,7 +22,7 @@
 	  
 	  function dropdown_menu($data='',$class=null,$id=null){
 	  
-	      $dropdown = '<ul class="dropdown-menu'.( ($class) ? ' '.$class : null ).'"'.( ($id) ? ' id="'.$id.'"' : null ).' rolej="menu"">';
+	      $dropdown = '<ul class="dropdown-menu'.( ($class) ? ' '.$class : null ).'"'.( ($id) ? ' id="'.$id.'"' : null ).' role="menu"">';
 		  
 	      if($data) { 
 		   
@@ -34,7 +32,7 @@
 					     $dropdown .= '<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">'.$menux.'<span class="caret"></span></a><ul class="dropdown-menu" role="menu">';
 						 
                           foreach($url as $value => $url ){
-						      $dropdown .= ( !empty($value) ? '<li><a href="'.$url.'">'.$value.'</a></li>' : '<li class="divider"></li>');
+						      $dropdown .= ( ($value) ? '<li><a href="'.$url.'">'.$value.'</a></li>' : '<li class="divider"></li>');
 		                  }
 						  
 		                 $dropdown .= '</ul></li>';
@@ -60,8 +58,7 @@
 		  - $class & $id is additional, can be empty
 		  
 	  */
-	  
-	  
+	    
 	  
 	  function button_groups($data='',$class=null,$id=null) {
 		 
@@ -77,19 +74,44 @@
 		 return $button_groups;
 	  }
 	  
+	  
+	  /* Select Dropdown */
+	  
+	  function select_dropdown($selectname='',$options,$current) {
+         $select = '<select name="'.$selectname.'">';	  
+	     foreach ($options as $option) {
+		     if( $option === $current ) {$select .= '<option value="'.$option.'" selected="selected">'.$option.'</option>';}
+		     else {$select .= '<option value="'.$option.'">'.$option.'</option>';}
+	     }	 
+		 $select .= '</select>';
+	     return $select;
+	  }
+	  
 	  /* ____ ....  ____ 
 	  
 	    ETC.. Sofar
 		
 	  function function_name($data,$class=null,$id=null) {
-	     $navbar = '';		    
-		 $navbar .= '</ul></div></div></nav>';
-		 return $navbar;
+		 return $xx;
 	  }
 	  
 	  */
-  
-  
-  
+	  
+	  
+	  function bootstrap_css($add_custom=false){
+	    if( !defined('STATIC_LIB') ) handle_this('Requitments','Plese define the STATIC_LIB on your Setting"s File.');
+		$CSS = Array(STATIC_LIB.'bootstrap/css/bootstrap.min.css'); $sty = '';
+		if(is_array($add_custom)) { foreach ($add_custom as $cus) { $CSS[] = $cus; } } else { if($add_custom) $CSS[] = $add_custom; }
+		 foreach ($CSS as $css){  $sty .= '<link href="'.$css.'" rel="stylesheet"/>'; }
+		return $sty;
+	  }
+	  
+	  function bootstrap_js($add_custom=false){
+	    if( !defined('STATIC_LIB') ) handle_this('Requitments','Plese define the STATIC_LIB on your Setting"s File.');
+		$JS = Array(STATIC_LIB.'jquery/jquery.min.js',STATIC_LIB.'bootstrap/js/bootstrap.min.js'); $script = '';
+		if(is_array($add_custom)) { foreach ($add_custom as $src) { $JS[] = $src; } } else { if($add_custom) $JS[] = $add_custom; }
+		 foreach ($JS as $js){  $script .= '<script src="'.$js.'"></script>'; }
+		return $script;
+	  }
   
   }
