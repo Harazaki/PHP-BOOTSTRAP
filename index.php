8ebdcdd9935f26be3412faa7ff765318ac29c677
Septyaman Vom <im@septyaman.com>
2014-12-28 23:06:09 +0700

    Add index file
    Index of document

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php define('HARAZAKIDA',TRUE);
+
+ /* 
+     * OPEN SORCE PROJECTS © HARAZAKI 2014 
+	   @PHP BOOOTSRAP PACKAGES
+	   About: index-of aplication 
+	 * Source https://github.com/Harazaki/PHP-BOOTSTRAP
+	 
+ */
+ 
+ // load the base, by default at root document of app
+ require_once('base_init.php');
+ 
+ // Set the app, where FUNC_BASIC exist,
+ if( defined('FUNC_BASIC') ) set_php_bootstrap();
