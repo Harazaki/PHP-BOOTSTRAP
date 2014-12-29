af625c91705b0b800c9ec8b015b947bbfa723adb
Septyaman Vom <im@septyaman.com>
2014-12-30 03:37:27 +0700

    Add Lang Example

---
+++ SET_/LANG_/BAHASA INDONESIA.php
@@ -0,0 +1,26 @@
+<?php if( !defined('HARAZAKIDA') ) exit;
+
+ /* 
+     * The PHP BOOTSTRAP LANG
+	 LANG name: Bahasa Indonesia
+	 Translate by Orang Indonesia
+	 Latest Update : DEC 21, 2014
+     source https://github.com/Harazaki/PHP-BOOTSTRAP
+	 $LANG_ Must be a array.
+ */
+ 
+ $LANG_ = array(
+   //Terjemahan Menu Navigasi
+ 'Get Started' => 'Mari Memulai !',
+ 'Example' => 'Contoh Aplikasi ',
+ 'Overview' => 'Ikhtisar',
+ 'Docs' => 'Dokumentasi',
+ 'Hello Word' => 'Hallo Dunia !',
+ 'Another Page' => 'Halaman Lain',
+ 'Custom Pages' => 'Halaman Kostum',
+ 'Mobile Layout' => 'Tampilan Ponsel',
+ 'Tablet Layout' => 'Tampilan Tablet',
+ 'Desktop Layout' => 'Tampilan Desktop',
+ 'Another Languange' => 'Multi Bahasa',
+ 'All Right reserved' => 'Hak Cipta Dilindungi UU',
+ );
---
+++ SET_/LANG_/ENGLISH.php
@@ -0,0 +1,14 @@
+<?php if( !defined('HARAZAKIDA') ) exit;
+
+ /* 
+     PHP BOOTSTRAP LANG
+	 LANG NAME = ENGLISH
+	 Translator = Mr.Tempe
+     source https://github.com/Harazaki/PHP-BOOTSTRAP
+	 $LANG_ Must be a array.
+ */
+ 
+ $LANG_ = array(
+ 'Get Started' => 'Get STARTED !',
+ 'Example' => 'Example ',
+ );
