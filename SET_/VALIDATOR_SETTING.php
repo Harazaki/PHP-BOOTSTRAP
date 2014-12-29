eed17e8b119fcb3db9e3da968ddd0d392189a3d1
Septyaman Vom <im@septyaman.com>
2014-12-30 03:38:45 +0700

    Add Validator Settings File

---
+++ SET_/VALIDATOR_SETTING.php
@@ -0,0 +1,19 @@
+<?php if( !defined('HARAZAKIDA') || !defined('FUNC_BASIC') ) exit('FUNC_BASIC are Reqruitments');
+
+  /* 
+        OPEN SOURCE PROJECTS © HARAZAKI 2014 
+	     * REGEX LIBRARY: The filter data regex expression
+	     - Learn more about at https://github.com/Harazaki/PHP-BOOTSTRAP
+  */
+  
+  $REGEX_VALIDATOR = array(
+      
+	  'coming soon' => '',
+  
+      );
+
+  $REGEX_CLEANER = array(
+      
+	  'coming soon' => '',
+
+     );
