2b875b32f8f8f0505422ad9e5b0689e8757cc7cf
Septyaman Vom <im@septyaman.com>
2015-01-02 00:46:02 +0700

    Fix

--- SET_/CONTENT_/index.php
+++ SET_/CONTENT_/index.php
@@ -3,9 +3,8 @@
  // Current's Page generate by simple CODEX, Like This :
   
  $mycontent = new PHP_BOOTSTRAP;
- 
  $mycontent->title = 'Hello Word!';
  
- $content = '<div class="container"><h1 class="heading">Hello Word, <small>Welcome To My '.APP_NAME.'</small></h1><hr/><p>How to build a pages on a simple way? Like This: </p><pre>'.highlight_file(D_CONTENT.'index.php',TRUE).'</pre></div>';
+ $content = '<div class="container"><h1 class="heading">Hello Word, <small>Welcome To My '.APP_NAME.'</small></h1><hr/><p>How to build a pages on a simple way? Like This:</p><pre>'.highlight_file(D_CONTENT.'index.php',TRUE).'</pre></div>';
  
  echo($mycontent->display($content));
